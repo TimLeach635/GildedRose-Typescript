40d79aa99943a29f793b0605396220fbce5c856c
Tim Leach <timleach635@gmail.com>
2022-01-27 17:31:28 +0000

    Add some semicolons

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -26,20 +26,20 @@ export class GildedRose {
 
             if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
                 if (this.items[i].quality > 0) {
-                    this.items[i].quality = this.items[i].quality - 1
+                    this.items[i].quality = this.items[i].quality - 1;
                 }
             } else {
                 if (this.items[i].quality < 50) {
-                    this.items[i].quality = this.items[i].quality + 1
+                    this.items[i].quality = this.items[i].quality + 1;
                     if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
                         if (this.items[i].sellIn < 11) {
                             if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1
+                                this.items[i].quality = this.items[i].quality + 1;
                             }
                         }
                         if (this.items[i].sellIn < 6) {
                             if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1
+                                this.items[i].quality = this.items[i].quality + 1;
                             }
                         }
                     }
@@ -50,14 +50,14 @@ export class GildedRose {
                 if (this.items[i].name != 'Aged Brie') {
                     if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
                         if (this.items[i].quality > 0) {
-                            this.items[i].quality = this.items[i].quality - 1
+                            this.items[i].quality = this.items[i].quality - 1;
                         }
                     } else {
-                        this.items[i].quality = this.items[i].quality - this.items[i].quality
+                        this.items[i].quality = this.items[i].quality - this.items[i].quality;
                     }
                 } else {
                     if (this.items[i].quality < 50) {
-                        this.items[i].quality = this.items[i].quality + 1
+                        this.items[i].quality = this.items[i].quality + 1;
                     }
                 }
             }
