7c94f1d875f5929965c59029a9dd06578ea203b1
Tim Leach <timleach635@gmail.com>
2022-01-27 17:41:11 +0000

    Add 'adjustedQuality' function

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -12,12 +12,26 @@ export class Item {
 
 export class GildedRose {
     items: Array<Item>;
+    minQuality: number = 0;
+    maxQuality: number = 50;
 
     constructor(items = [] as Array<Item>) {
         this.items = items;
     }
 
     updateQuality() {
+        // Changes quality by an amount, but doesn't go outside of the quality bounds
+        const adjustedQuality = (quality: number, amount: number): number => {
+            const newQuality = quality + amount;
+            if (newQuality < this.minQuality) {
+                return this.minQuality;
+            }
+            if (newQuality > this.maxQuality) {
+                return this.maxQuality;
+            }
+            return newQuality;
+        }
+
         for (let i = 0; i < this.items.length; i++) {
             // Sulfuras never changes
             if (this.items[i].name === "Sulfuras, Hand of Ragnaros") {
@@ -25,22 +39,16 @@ export class GildedRose {
             }
 
             if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if (this.items[i].quality > 0) {
-                    this.items[i].quality = this.items[i].quality - 1;
-                }
+                this.items[i].quality = adjustedQuality(this.items[i].quality, -1);
             } else {
                 if (this.items[i].quality < 50) {
                     this.items[i].quality = this.items[i].quality + 1;
                     if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
                         if (this.items[i].sellIn < 11) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1;
-                            }
+                            this.items[i].quality = adjustedQuality(this.items[i].quality, 1);
                         }
                         if (this.items[i].sellIn < 6) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1;
-                            }
+                            this.items[i].quality = adjustedQuality(this.items[i].quality, 1);
                         }
                     }
                 }
@@ -49,16 +57,12 @@ export class GildedRose {
             if (this.items[i].sellIn < 0) {
                 if (this.items[i].name != 'Aged Brie') {
                     if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (this.items[i].quality > 0) {
-                            this.items[i].quality = this.items[i].quality - 1;
-                        }
+                        this.items[i].quality = adjustedQuality(this.items[i].quality, -1);
                     } else {
                         this.items[i].quality = this.items[i].quality - this.items[i].quality;
                     }
                 } else {
-                    if (this.items[i].quality < 50) {
-                        this.items[i].quality = this.items[i].quality + 1;
-                    }
+                    this.items[i].quality = adjustedQuality(this.items[i].quality, 1);
                 }
             }
         }
